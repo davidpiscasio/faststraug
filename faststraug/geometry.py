3c01d3194345dfed145c946b0eb0ed095aa6a52e
David Piscasio <82606289+davidpiscasio@users.noreply.github.com>
2023-04-20 20:37:36 +0800

    Add Shrink function to geometry.py

--- faststraug/geometry.py
+++ faststraug/geometry.py
@@ -1,6 +1,84 @@
 import torchvision.transforms as transforms
 import numpy as np
-from einops import rearrange
+import torch
+import kornia
+
+class Shrink:
+    def __init__(self, rng=None):
+        self.translateXAbs = TranslateXAbs()
+        self.translateYAbs = TranslateYAbs()
+
+    def __call__(self, img, mag=-1, prob=1.):
+        if np.random.uniform(0, 1) > prob:
+            return img
+
+        w, h = img.size
+        img = transforms.ToTensor()(img).to('cuda')
+        img = torch.unsqueeze(img,0)
+
+        w_33 = 0.33 * w
+        w_50 = 0.50 * w
+        w_66 = 0.66 * w
+
+        h_50 = 0.50 * h
+
+        p = 0
+
+        # frac = 0.4
+
+        b = [.2, .3, .4]
+        if mag < 0 or mag >= len(b):
+            index = 0
+        else:
+            index = mag
+        frac = b[index]
+
+        srcpt = np.zeros((1,8,2))
+        dstpt = np.zeros((1,8,2))
+
+        # left-most
+        srcpt[0,0,:] = [p, p]
+        srcpt[0,1,:] = [p, h - p]
+        x = np.random.uniform(frac - .1, frac) * w_33
+        y = np.random.uniform(frac - .1, frac) * h_50
+        dstpt[0,0,:] = [p + x, p + y]
+        dstpt[0,1,:] = [p + x, h - p - y]
+
+        # 2nd left-most 
+        srcpt[0,2,:] = [p + w_33, p]
+        srcpt[0,3,:] = [p + w_33, h - p]
+        dstpt[0,2,:] = [p + w_33, p + y]
+        dstpt[0,3,:] = [p + w_33, h - p - y]
+
+        # 3rd left-most 
+        srcpt[0,4,:] = [p + w_66, p]
+        srcpt[0,5,:] = [p + w_66, h - p]
+        dstpt[0,4,:] = [p + w_66, p + y]
+        dstpt[0,5,:] = [p + w_66, h - p - y]
+
+        # right-most 
+        srcpt[0,6,:] = [w - p, p]
+        srcpt[0,7,:] = [w - p, h - p]
+        dstpt[0,6,:] = [w - p - x, p + y]
+        dstpt[0,7,:] = [w - p - x, h - p - y]
+
+        srcpt = torch.from_numpy(srcpt).to('cuda')
+        dstpt = torch.from_numpy(dstpt).to('cuda')
+
+        srcpt[:,:,0] = 2.0*(srcpt[:,:,0]/w) - 1.0
+        dstpt[:,:,0] = 2.0*(dstpt[:,:,0]/w) - 1.0
+        srcpt[:,:,1] = 2.0*(srcpt[:,:,1]/h) - 1.0
+        dstpt[:,:,1] = 2.0*(dstpt[:,:,1]/h) - 1.0
+
+        kernel_weights, affine_weights = kornia.geometry.transform.get_tps_transform(dstpt, srcpt)
+        img = kornia.geometry.transform.warp_image_tps(img.double(), srcpt, kernel_weights, affine_weights, align_corners=True)
+
+        if np.random.uniform(0, 1) < 0.5:
+            img = self.translateXAbs(img, val=x)
+        else:
+            img = self.translateYAbs(img, val=y)
+
+        return torch.squeeze(img)
 
 class Perspective:
     def __call__(self, img, mag=-1, prob=1.):
@@ -61,3 +139,28 @@ class Rotate:
         img = transforms.Resize(size=(h, w), interpolation=transforms.InterpolationMode.BICUBIC, antialias=True)(img)
 
         return img
+    
+class TranslateXAbs:
+    def __call__(self, img, val=0, prob=1.):
+        if np.random.uniform(0, 1) > prob:
+            return img
+
+        v = np.random.uniform(0, val)
+
+        if np.random.uniform(0, 1) > 0.5:
+            v = -v
+        #return img.transform(img.size, Image.AFFINE, (1, 0, v, 0, 1, 0))
+        return kornia.geometry.transform.warp_affine(img, M=[[[1, 0, v],[0, 1, 0]]], dsize=img.shape[2:])
+
+
+class TranslateYAbs:
+    def __call__(self, img, val=0, prob=1.):
+        if np.random.uniform(0, 1) > prob:
+            return img
+
+        v = np.random.uniform(0, val)
+
+        if np.random.uniform(0, 1) > 0.5:
+            v = -v
+        #return img.transform(img.size, Image.AFFINE, (1, 0, 0, 0, 1, v))
+        return kornia.geometry.transform.warp_affine(img, M=[[[1, 0, 0],[0, 1, v]]], dsize=img.shape[2:])
