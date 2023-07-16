64566bf9ad263d6a8fda1f3fe178b01e720f2b55
David Piscasio <82606289+davidpiscasio@users.noreply.github.com>
2023-07-16 20:55:09 +0800

    Update functions to allow both tensor and image inputs

--- faststraug/noise.py
+++ faststraug/noise.py
@@ -10,7 +10,15 @@ class GaussianNoise:
         if np.random.uniform(0, 1) > prob:
             return transforms.ToTensor()(img).to('cuda')
 
-        n_channels = len(img.getbands())
+        if torch.is_tensor(img):
+            img = torch.unsqueeze(img, 0) if img.ndim == 2 else img
+            n_channels = img.shape[0]
+            #img = transforms.ToPILImage()(img)
+            img = cp.squeeze(rearrange(cp.asarray(img), 'c h w -> h w c'))
+        else:
+            n_channels = len(img.getbands())
+            img = cp.asarray(img) / 255.
+            
         isgray = n_channels == 1
 
         b = [.06, 0.09, 0.12]
@@ -20,20 +28,35 @@ class GaussianNoise:
             index = mag
         a = b[index]
         c = np.random.uniform(a, a + 0.03)
-        img = cp.asarray(img) / 255.
+        
+        #img = cp.asarray(img) / 255.
         img = cp.clip(img + cp.random.normal(size=img.shape, scale=c), 0, 1)
 
-        if not isgray:
+        if isgray and img.ndim == 2:
+            img = cp.expand_dims(img, axis=0)
+        else:
             img = rearrange(img, 'h w c -> c h w')
+            
+        img = torch.as_tensor(img, device='cuda')
+            
+        if img.shape[0] != 1 and isgray:
+            img = transforms.functional.rgb_to_grayscale(img)
 
-        return torch.as_tensor(img, device='cuda')
+        return img
 
 class ShotNoise:
     def __call__(self, img, mag=-1, prob=1.):
         if np.random.uniform(0, 1) > prob:
             return transforms.ToTensor()(img).to('cuda')
 
-        n_channels = len(img.getbands())
+        if torch.is_tensor(img):
+            img = torch.unsqueeze(img, 0) if img.ndim == 2 else img
+            n_channels = img.shape[0]
+            #img = transforms.ToPILImage()(img)
+            img = cp.squeeze(rearrange(cp.asarray(img), 'c h w -> h w c'))
+        else:
+            n_channels = len(img.getbands())
+            img = cp.asarray(img) / 255.
         isgray = n_channels == 1
 
         b = [13, 8, 3]
@@ -43,20 +66,34 @@ class ShotNoise:
             index = mag
         a = b[index]
         c = np.random.uniform(a, a + 7)
-        img = cp.asarray(img) / 255.
+        #img = cp.asarray(img) / 255.
         img = cp.clip(cp.random.poisson(img * c) / float(c), 0, 1)
 
-        if not isgray:
+        if isgray and img.ndim == 2:
+            img = cp.expand_dims(img, axis=0)
+        else:
             img = rearrange(img, 'h w c -> c h w')
+            
+        img = torch.as_tensor(img, device='cuda')
+            
+        if img.shape[0] != 1 and isgray:
+            img = transforms.functional.rgb_to_grayscale(img)
 
-        return torch.as_tensor(img, device='cuda')
+        return img
 
 class ImpulseNoise:
     def __call__(self, img, mag=-1, prob=1.):
         if np.random.uniform(0, 1) > prob:
             return transforms.ToTensor()(img).to('cuda')
 
-        n_channels = len(img.getbands())
+        if torch.is_tensor(img):
+            img = torch.unsqueeze(img, 0) if img.ndim == 2 else img
+            n_channels = img.shape[0]
+            #img = transforms.ToPILImage()(img)
+            img = cp.squeeze(rearrange(cp.asarray(img), 'c h w -> h w c'))
+        else:
+            n_channels = len(img.getbands())
+            img = cp.asarray(img) / 255.
         isgray = n_channels == 1
 
         b = [.03, .07, .11]
@@ -67,19 +104,33 @@ class ImpulseNoise:
         a = b[index]
         c = np.random.uniform(a, a + .04)
         s = np.random.randint(2 ** 32, size=4)
-        img = cc.skimage.util.random_noise(cp.asarray(img) / 255., mode='s&p', seed=s, amount=c)
+        img = cc.skimage.util.random_noise(img, mode='s&p', seed=s, amount=c)
 
-        if not isgray:
+        if isgray and img.ndim == 2:
+            img = cp.expand_dims(img, axis=0)
+        else:
             img = rearrange(img, 'h w c -> c h w')
+            
+        img = torch.as_tensor(img, device='cuda')
+            
+        if img.shape[0] != 1 and isgray:
+            img = transforms.functional.rgb_to_grayscale(img)
 
-        return torch.as_tensor(img, device='cuda')
+        return img
         
 class SpeckleNoise:
     def __call__(self, img, mag=-1, prob=1.):
         if np.random.uniform(0, 1) > prob:
             return transforms.ToTensor()(img).to('cuda')
 
-        n_channels = len(img.getbands())
+        if torch.is_tensor(img):
+            img = torch.unsqueeze(img, 0) if img.ndim == 2 else img
+            n_channels = img.shape[0]
+            #img = transforms.ToPILImage()(img)
+            img = cp.squeeze(rearrange(cp.asarray(img), 'c h w -> h w c'))
+        else:
+            n_channels = len(img.getbands())
+            img = cp.asarray(img) / 255.
         isgray = n_channels == 1
 
         b = [.15, .2, .25]
@@ -89,10 +140,17 @@ class SpeckleNoise:
             index = mag
         a = b[index]
         c = np.random.uniform(a, a + .05)
-        img = cp.asarray(img) / 255.
+        #img = cp.asarray(img) / 255.
         img = cp.clip(img + img * cp.random.normal(size=img.shape, scale=c), 0, 1)
 
-        if not isgray:
+        if isgray and img.ndim == 2:
+            img = cp.expand_dims(img, axis=0)
+        else:
             img = rearrange(img, 'h w c -> c h w')
+            
+        img = torch.as_tensor(img, device='cuda')
+            
+        if img.shape[0] != 1 and isgray:
+            img = transforms.functional.rgb_to_grayscale(img)
 
-        return torch.as_tensor(img, device='cuda')
+        return img
